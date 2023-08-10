ffc59377ed6586218946c6b60f3e0833f27f179d
Pablo Tesone <tesonep@gmail.com>
2023-08-10 16:50:23 +0200

    improving process

--- buildOSX.sh
+++ buildOSX.sh
@@ -6,10 +6,18 @@ set -e
 wget -O -  get.pharo.org/64/110+vm | bash
 ./pharo Pharo.image save Takuzu
 
-./pharo Takuzu.image eval --save "Metacello new
-    baseline:'Takuzu';
-    repository: 'github://tesonep/Takuzu:main/src';
-    load: 'core'"
+if [ -v GITHUB_WORKSPACE ] 
+then
+	./pharo Takuzu.image eval --save Metacello new \
+	    baseline:'Takuzu'; \
+	    repository: 'gitlocal://$GITHUB_WORKSPACE/src';\
+	    load: 'core'
+else
+	./pharo Takuzu.image eval --save "Metacello new
+	    baseline:'Takuzu';
+	    repository: 'github://tesonep/Takuzu:main/src';
+	    load: 'core'"	
+fi
 
 ./pharo Takuzu.image eval "Takuzu generateOSXPackage"
 
