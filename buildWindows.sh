59ab09b7770d180d33ff827e72c11000e6026324
Pablo Tesone <tesonep@gmail.com>
2023-08-17 15:08:50 +0200

    Adding scripts and fixing line endings

---
+++ buildWindows.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -x
+set -e
+
+wget -O -  get.pharo.org/64/110+vm | bash
+./pharo Pharo.image save Takuzu
+
+if [ ! -z "$GITHUB_WORKSPACE" ] 
+then
+	./pharo Takuzu.image eval --save "Metacello new 
+	    baseline:'Takuzu'; 
+	    repository: 'gitlocal://$GITHUB_WORKSPACE/src';
+	    load: 'core'"
+else
+	./pharo Takuzu.image eval --save "Metacello new
+	    baseline:'Takuzu';
+	    repository: 'github://tesonep/Takuzu:main/src';
+	    load: 'core'"	
+fi
+
+./pharo Takuzu.image eval "Takuzu generateWindowsPackage"
+
+cd build
+cmake -B . -S . && cmake --build .
+powershell -File buildWindowsPackage.ps1
